80eb96b0985429e2be5a60cfaaa030b6a9a55de9
esbenlykke <sesben@hotmail.com>
2023-02-06 12:36:11 +0100

    rename some files

--- code/evaluate_models/create_roc_data.R
+++ code/evaluate_models/create_roc_data.R
@@ -38,26 +38,3 @@ map_dfr(in_bed_fits, ~ get_roc(.x, in_bed, .pred_1), .id = "model") |>
 map_dfr(sleep_fits, ~ get_roc(.x, in_bed, .pred_1), .id = "model") |> 
     write_parquet("data/processed/roc_data_sleep.parquet")
 
-
-# ROC curve plots ---------------------------------------------------------
-
-
-in_bed_roc |>
-  ggplot(
-    aes(1 - specificity, sensitivity, color = model)
-  ) + # plot with 2 ROC curves for each model
-  geom_line(linewidth = .8, alpha = .7) +
-  geom_abline(slope = 1, intercept = 0, linewidth = 0.4, lty = 2) +
-  scale_color_brewer(palette = "Dark2") +
-  coord_fixed() +
-  theme_light()
-
-sleep_roc |>
-  ggplot(
-    aes(1 - specificity, sensitivity, color = model)
-  ) + # plot with 2 ROC curves for each model
-  geom_line(linewidth = .8, alpha = .7) +
-  geom_abline(slope = 1, intercept = 0, linewidth = 0.4, lty = 2) +
-  scale_color_brewer(palette = "Dark2") +
-  coord_fixed() +
-  theme_light()
