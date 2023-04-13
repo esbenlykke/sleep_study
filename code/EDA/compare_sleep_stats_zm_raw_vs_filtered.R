11c652c5dd51e36decbfeab73eae4bc93f92c100
Esben Lykke <sesben@hotmail.com>
2023-04-13 11:47:06 +0200

    restore

---
+++ code/EDA/compare_sleep_stats_zm_raw_vs_filtered.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(furrr)
+library(slider)
+library(arrow)
+library(lubridate)
+
+zm_raw <-
+  read_tsv("data/processed/zm_scores.tsv")
+
+zm_stats <- 
+  read_csv("data/processed/all_zm_stats.csv")
+
+
+zm_raw %>%
+  # filter(score != -5) %>% 
+  mutate(
+    noon_day = day(datetime - hours(12)),
+    sleep = if_else(score != 0, 1, 0),
+    sleep_24 = slide_dbl(sleep, sum, .after = 24),
+    waso = slide_lgl(sleep, ~ sum(.x) == 0, .before = 3)
+  ) |> #filter(id == 8504 & noon_day == 14) %>% view()
+  group_by(id, noon_day) |>
+  summarise(
+    spt_hrs = n() * 30 / 60 / 60,
+    tst_hrs = sum(sleep) * 30 / 60 / 60,
+    se_percent = 100 * (tst_hrs / spt_hrs),
+    row = min(row_number()[sleep_24 == 20]) + 4,
+    lps_min = (min(row_number()[sleep_24 == 20]) + 4) * 30 / 60,
+    waso_min = (sum(waso) * 30) / 60,
+    .groups = "drop"
+  ) %>% 
+  filter(id == 8504)
+
+zm_filtered <- 
+  zm_raw %>%
+  # filter(id == 8504) %>% 
+  mutate(
+    score_filtered = slide_dbl(score, median, .after = 10)
+  )
+
+# write_parquet(zm_filtered, "data/processed/all_zm_stats_median_filtered_10_min.parquet")
+
+# plot raw score vs filtered score
+zm_filtered %>% 
+  filter(day == 0) %>% 
+  ggplot() +
+  geom_step(aes(datetime, score), alpha = .5) +
+  geom_step(aes(datetime, score_filtered), color = "red") +
+  theme_light()
+
+zm_filtered %>% 
+  # filter(score != -5) %>%
+  mutate(
+    noon_day = day(datetime - hours(12)),
+    score_filtered = slide_dbl(score, median, .after = 10),
+    sleep = if_else(score_filtered != 0, 1, 0),
+    sleep_24 = slide_dbl(sleep, sum, .after = 24),
+    waso = slide_lgl(sleep, ~ sum(.x) == 0, .before = 3)
+  ) |> #filter(id == 8504 & noon_day == 14) %>% view()
+  group_by(id, noon_day) |>
+  summarise(
+    spt_hrs = n() * 30 / 60 / 60,
+    tst_hrs = sum(sleep) * 30 / 60 / 60,
+    se_percent = 100 * (tst_hrs / spt_hrs),
+    row = min(row_number()[sleep_24 == 20]) + 4,
+    lps_min = (min(row_number()[sleep_24 == 20]) + 4) * 30 / 60,
+    waso_min = (sum(waso) * 30) / 60,
+    .groups = "drop"
+  ) %>% 
+  filter(id == 8504)
+
+
+zm_stats %>% filter(id == 8504)
---
+++ code/create_models/predict_and_extract_in_bed_data.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(tidymodels)
+library(arrow)
+library(slider)
+
+test <-
+  read_parquet("data/data_for_modelling/crude_testing_data.parquet") %>%
+  mutate(
+    in_bed_filtered = slider::slide_dbl(as.numeric(.pred_class) - 1, median, .after = 50, .before = 50),
+  )
+
+in_bed_fit <-
+  read_rds("/media/esbenlykke/My Passport/crude/fitted_models/axed_models/decision_tree_in_bed_AXED.rds")
+
+sleep_fit <-
+  read_rds("/media/esbenlykke/My Passport/crude/fitted_models/axed_models/decision_tree_sleep_median_AXED.rds")
+
+get_in_bed_data <- function(in_bed_fit, sleep_fit) {
+  in_bed_data <-
+    fit %>%
+    augment(test) %>%
+    group_by(id, noon_day) %>%
+    group_modify(~ .x %>%
+      filter(row_number() > min(row_number()[in_bed_filtered == 1]) &
+        row_number() < max(row_number()[in_bed_filtered == 1])))
+
+  sleep_data <-
+    sleep_fit %>%
+    augment(in_bed_data)
+}
+
+
+in_bed_fit %>%
+  augment(test) %>%
+  group_by(id, noon_day) %>%
+  group_modify(~ .x %>%
+    filter(row_number() > min(row_number()[in_bed_filtered == 1]) &
+      row_number() < max(row_number()[in_bed_filtered == 1])))
