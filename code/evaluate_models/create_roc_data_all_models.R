87da421305c472ac272bc000a9379f88e26adabd
Esben Lykke <sesben@hotmail.com>
2023-05-02 22:04:21 +0200

    prepare roc auc data

--- code/evaluate_models/create_roc_data_all_models.R
+++ code/evaluate_models/create_roc_data_all_models.R
@@ -6,104 +6,168 @@ library(arrow)
 
 
 get_roc <-
-  function(fit, truth, estimate) {
+  function(fit, test, truth, estimate) {
     fit |>
       augment(test) |>
       roc_curve(truth = {{ truth }}, estimate = {{ estimate }}, event_level = "second")
   }
 
+get_roc_auc <-
+  function(fit, test, truth, estimate) {
+    fit |>
+      augment(test) |>
+      roc_auc(truth = {{ truth }}, estimate = {{ estimate }}, event_level = "second")
+  }
 
-# CRUDE -------------------------------------------------------------------
-
-
-fits_10_paths <- 
-  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows",
-  full.names = TRUE, recursive = TRUE
-) %>% 
-  str_subset("10_sec")
-
-fits_30_paths <- 
-  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows",
-             full.names = TRUE, recursive = TRUE
-  ) %>% 
+## Get the file names of the fitted models for the 10-second epochs, grouped by preprocessing type (raw, median5, and median10)
+fits_10_filenames_raw <- 
+  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows/sleep",
+             full.names = TRUE) %>% 
+  str_subset("raw") %>% 
   str_subset("30_sec")
 
 
-fits_10 <- map(fits_10_paths, read_rds) |>
-  set_names(c("decision_tree", "logistic_regression", "neural_net", "xgboost"))
-
-fits_30 <- map(fits_30_paths, read_rds) 
-
-test <- read_parquet("data/data_for_modelling/chained_classifiers/testing_30_sec_data.parquet") %>% 
-  mutate(across(contains("sleep"), as_factor))
+fits_10_filenames_median5 <- 
+  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows/sleep",
+             full.names = TRUE) %>% 
+  str_subset("5_min") %>% 
+  str_subset("30_sec")
 
 
-map_dfr(fits_10, ~ get_roc(.x, in_bed, .pred_1), .id = "model") |>
-  write_parquet("data/processed/crude_roc_data_in_bed.parquet")
-
-map_dfr(fits_30, ~ get_roc(.x, in_bed, .pred_1), .id = "model") |>
-  write_parquet("data/processed/")
-
-
-# Binary relevance --------------------------------------------------------
+fits_10_filenames_median10 <- 
+  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows/sleep",
+             full.names = TRUE) %>% 
+  str_subset("10_min") %>% 
+  str_subset("30_sec")
 
 
-in_bed_asleep_files <- list.files("/media/esbenlykke/My Passport/binary_relevance/fitted_models/axed_models",
-  full.names = TRUE
-) %>%
-  str_subset("asleep")
+# Read the models from the file names and store them in separate lists
+fits_10_raw <- map(fits_10_filenames_raw, read_rds)
+fits_10_median5 <- map(fits_10_filenames_median5, read_rds)
+fits_10_median10 <- map(fits_10_filenames_median10, read_rds)
 
-in_bed_awake_files <- list.files("/media/esbenlykke/My Passport/binary_relevance/fitted_models/axed_models",
-  full.names = TRUE
-) %>%
-  str_subset("in_bed_awake")
+# Repeat the same process for the 30-second epochs
+fits_30_filenames_raw <- 
+  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows/sleep",
+             full.names = TRUE) %>% 
+  str_subset("raw") %>% 
+  str_subset("30_sec")
 
-out_bed_awake_files <- list.files("/media/esbenlykke/My Passport/binary_relevance/fitted_models/axed_models",
-  full.names = TRUE
-) %>%
-  str_subset("out_bed_awake")
+fits_30_filenames_median5 <- 
+  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows/sleep",
+             full.names = TRUE) %>% 
+  str_subset("5_min") %>% 
+  str_subset("30_sec")
 
-in_bed_asleep_fits <- map(in_bed_asleep_files, read_rds) |>
-  set_names(c("decision_tree", "logistic_regression", "neural_net", "xgboost"))
+fits_30_filenames_median10 <- 
+  list.files("/media/esbenlykke/My Passport/chained_models/fitted_workflows/sleep",
+             full.names = TRUE) %>% 
+  str_subset("10_min") %>% 
+  str_subset("30_sec")
 
-in_bed_awake_fits <- map(in_bed_awake_files, read_rds) |>
-  set_names(c("decision_tree", "logistic_regression", "neural_net", "xgboost"))
+# Read the models from the file names and store them in separate lists
+fits_30_raw <- map(fits_30_filenames_raw, read_rds)
+fits_30_median5 <- map(fits_30_filenames_median5, read_rds)
+fits_30_median10 <- map(fits_30_filenames_median10, read_rds)
 
-out_bed_awake_fits <- map(out_bed_awake_files, read_rds) |>
-  set_names(c("decision_tree", "logistic_regression", "neural_net", "xgboost"))
+# Read the test datasets for the 10-second and 30-second epochs
+test_10 <-
+  read_parquet("data/data_for_modelling/chained_classifiers/only_in_bed_testing_data_10_sec_epochs.parquet") %>% 
+  select(-contains(".pred"))
 
-test <- read_parquet("data/data_for_modelling/binary_relevance_testing_data.parquet")
+test_30 <-
+  read_parquet("data/data_for_modelling/chained_classifiers/only_in_bed_testing_data_30_sec_epochs.parquet") %>% 
+  select(-contains(".pred"))
 
+model_names <- 
+  c("Decision Tree", "Logistic Regression", "Neural Network", "XGBoost")
 
-map_dfr(in_bed_asleep_fits, ~ get_roc(.x, in_bed_asleep, .pred_1), .id = "model") |>
-  write_parquet("data/processed/binary_relevance_roc_data_in_bed_asleep.parquet")
+roc_raw_10 <- 
+  fits_10_raw %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc(.x, test_10, sleep, .pred_1), .id = "model")
 
-map_dfr(in_bed_awake_fits, ~ get_roc(.x, in_bed_awake, .pred_1), .id = "model") |>
-  write_parquet("data/processed/binary_relevance_roc_data_in_bed_awake.parquet")
+roc_median5_10 <- 
+  fits_10_median5 %>%
+  set_names(model_names) %>%  
+  map_dfr(~ get_roc(.x, test_10, sleep_median5, .pred_1), .id = "model")
 
-map_dfr(out_bed_awake_fits, ~ get_roc(.x, out_bed_awake, .pred_1), .id = "model") |>
-  write_parquet("data/processed/binary_relevance_roc_data_out_bed_awake.parquet")
+roc_median10_10 <- 
+  fits_10_median10 %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc(.x, test_10, sleep_median10, .pred_1), .id = "model")
 
+all_roc_10 <- 
+  bind_rows(raw = roc_raw_10, median_5 = roc_median5_10, median_10 = roc_median10_10, 
+            .id = "group")
 
+# Save roc as CSV
+# all_roc_10 %>%
+#   write_parquet("presentation/data/all_roc_sleep_10.parquet")
 
-# Multiclass --------------------------------------------------------------
+roc_raw_30 <- 
+  fits_30_raw %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc(.x, test_30, sleep, .pred_1), .id = "model")
 
+roc_median5_30 <- 
+  fits_30_median5 %>%
+  set_names(model_names) %>%  
+  map_dfr(~ get_roc(.x, test_30, sleep_median5, .pred_1), .id = "model")
 
-filesnames <- list.files("/media/esbenlykke/My Passport/multiclass/fitted_models/axed_models",
-  full.names = TRUE
-)
+roc_median10_30 <- 
+  fits_30_median10 %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc(.x, test_30, sleep_median10, .pred_1), .id = "model")
 
-fits <- map(filesnames, read_rds) |>
-  set_names(c("decision_tree", "decision_tree_SMOTE", "logistic_regression", "neural_net", "xgboost"))
+all_roc_30 <- 
+  bind_rows(raw = roc_raw_30, median_5 = roc_median5_30, median_10 = roc_median10_30, 
+            .id = "group")
 
-test <- read_parquet("data/data_for_modelling/multiclass_testing_data.parquet") %>%
-  mutate(multiclass = factor(multiclass,
-    levels = c("in_bed_asleep", "in_bed_awake", "out_bed_awake")
-  ))
+# Save roc as CSV
+# all_roc_30 %>%
+  # write_parquet("presentation/data/all_roc_sleep_30.parquet")
 
+roc_auc_raw_10 <- 
+  fits_10_raw %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc_auc(.x, test_10, sleep, .pred_1), .id = "model")
 
-map_dfr(fits,
-  ~ get_roc(.x, multiclass, c(.pred_in_bed_asleep, .pred_in_bed_awake, .pred_out_bed_awake)),
-  .id = "model"
-) |>
-  write_parquet("data/processed/multiclass_OvsALL_roc_data.parquet")
+roc_auc_median5_10 <- 
+  fits_10_median5 %>%
+  set_names(model_names) %>%  
+  map_dfr(~ get_roc_auc(.x, test_10, sleep_median5, .pred_1), .id = "model")
+
+roc_auc_median10_10 <- 
+  fits_10_median10 %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc_auc(.x, test_10, sleep_median10, .pred_1), .id = "model")
+
+all_roc_auc_10 <- 
+  bind_rows(raw = roc_auc_raw_10, median_5 = roc_auc_median5_10, median_10 = roc_auc_median10_10, 
+            .id = "group")
+
+# all_roc_auc_10 %>% 
+#   write_csv("presentation/data/all_roc_auc_sleep_10_sec_epochs.csv")
+
+roc_auc_raw_30 <- 
+  fits_30_raw %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc_auc(.x, test_30, sleep, .pred_1), .id = "model")
+
+roc_auc_median5_30 <- 
+  fits_30_median5 %>%
+  set_names(model_names) %>%  
+  map_dfr(~ get_roc_auc(.x, test_30, sleep_median5, .pred_1), .id = "model")
+
+roc_auc_median10_30 <- 
+  fits_30_median10 %>% 
+  set_names(model_names) %>% 
+  map_dfr(~ get_roc_auc(.x, test_30, sleep_median10, .pred_1), .id = "model")
+
+all_roc_auc_30 <- 
+  bind_rows(raw = roc_auc_raw_30, median_5 = roc_auc_median5_30, median_10 = roc_auc_median10_30, 
+            .id = "group")
+
+# all_roc_auc_30 %>% 
+#     write_csv("presentation/data/all_roc_auc_sleep_30_sec_epochs.csv")
--- code/evaluate_models/plot_performance_metrics.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env RscriptsuppressMessages(library(tidyverse))
-
-library(tidyverse)
-
-
-metric_data <- read_csv("data/processed/performance_metrics.csv")
-
