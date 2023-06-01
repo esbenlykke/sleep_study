c5b8b6dfbd64f8b7721c82a0ce42b084bc58ddde
esbenlykke <sesben@hotmail.com>
2023-06-01 13:14:41 +0200

    sleep preds joined to original data, correctly, finallygit add .

--- code/evaluate_models/predict_and_extract_in_bed_data.R
+++ code/evaluate_models/predict_and_extract_in_bed_data.R
@@ -7,43 +7,37 @@ library(slider)
 library(lubridate)
 
 # Function to process data
-extract_in_bed_data <- function(model_filenames, data_path, epoch_length, output_dir) {
-  # Read data
-  data <- read_parquet(data_path)
-
-  # Extract model names from filenames
-  model_names <- str_match(model_filenames, ".*/(.*).rds$")
-
-  # Read fitted models
-  fits <- map(model_filenames, read_rds)
-
-  # Generate predictions
-  preds <- map(fits, ~ .x %>%
-    augment(data) %>%
+extract_in_bed_data <- function(model_filename, data_path, epoch_length, output_dir) {
+  # Extract model name from filename
+  model_name <- str_match(model_filename, ".*/(.*).rds$")[, 2]
+  
+  # Read fitted model
+  fit <- read_rds(model_filename)
+  
+  # Read data, generate predictions, and write data with predictions before mutate step
+   fit %>%
+    augment(read_parquet(data_path)) %>%
     rename_with(~ str_replace(.x, ".pred", "in_bed")) %>%
+    {write_parquet(., paste0("data/data_for_modelling/chained_classifiers/", model_name, "_predictions_complete.parquet"))
+      cat("Data for model", model_name, "before mutate has been written to", paste0("data/data_for_modelling/chained_classifiers/", model_name, "_predictions_complete.parquet"), "\n\n")
+      .} %>%
     mutate(
       in_bed_filtered = slide_dbl(as.numeric(in_bed_class) - 1, median,
-        .after = (7.5 * 60) / epoch_length, .before = (7.5 * 60) / epoch_length
+                                  .after = (7.5 * 60) / epoch_length, .before = (7.5 * 60) / epoch_length
       )
     ) %>%
     group_by(id, noon_day, month) %>%
     group_modify(~ .x %>%
-      # Filter rows where row_number is within the in_bed_filtered range
-      filter(row_number() > min(row_number()[in_bed_filtered == 1]) &
-        row_number() < max(row_number()[in_bed_filtered == 1]))) %>%
-    ungroup()) %>%
-    set_names(model_names[, 2])
-
-  # Write predictions to disk
-  walk2(preds, names(preds), \(pred, name) {
-    output_file <- paste0(output_dir, name, ".parquet")
-    write_parquet(pred, output_file)
-    cat("Extracted data for model", name, "has been written to", output_file, "\n\n")
-  })
-
-
-  return(preds)
+                   # Filter rows where row_number is within the in_bed_filtered range
+                   filter(row_number() > min(row_number()[in_bed_filtered == 1]) &
+                            row_number() < max(row_number()[in_bed_filtered == 1]))) %>%
+    ungroup() %>%
+    {write_parquet(., paste0(output_dir, model_name, ".parquet"))
+      cat("Extracted data for model", model_name, "has been written to", paste0(output_dir, model_name, ".parquet"), "\n\n")
+      .}
 }
+
+
 
 # Set common variables
 models_dir <- "/media/esbenlykke/My Passport/chained_models/fitted_workflows/in_bed"
@@ -56,6 +50,7 @@ data_dir <- list.files("data/data_for_modelling", full.names = TRUE) %>%
 output_dir <- "data/data_for_modelling/chained_classifiers/extracted_in_bed_data/"
 dir.create(output_dir, showWarnings = FALSE)
 
+# Process data and write extracted data to disk
 # Process data and write extracted data to disk
 preds_30 <- map(model_filenames, ~ extract_in_bed_data(.x,
   data_path = data_dir[2],
--- code/evaluate_models/predict_sleep_on_in_bed_data.R
+++ code/evaluate_models/predict_sleep_on_in_bed_data.R
@@ -7,41 +7,34 @@ library(slider)
 library(lubridate)
 
 predict_sleep <- function(model_filename, in_bed_data_filename) {
-  # Extract the prediction name from the model filename
-  pred_name <- str_extract(model_filename, "sleep_(.*?)_(.*?)_30_sec_epochs")
-  
-  # Load the model and augment the in-bed data
   read_rds(model_filename) %>%
     augment(read_parquet(in_bed_data_filename)) %>%
     select(id, noon_day, month, datetime, contains("pred_class"))
 }
 
 get_filenames <- function(path, pattern) {
-  # Get the filenames in a directory that match a specific pattern
   list.files(path, full.names = TRUE) %>% str_subset(pattern)
 }
 
 run_predictions <- function(model_filenames, data_filename) {
-  # Run predictions for each model on the given data filename
   map(model_filenames, predict_sleep, in_bed_data_filename = data_filename)
 }
 
 pair_filenames <- function(model_filenames, in_bed_data_filenames) {
-  # Extract model types from filenames
   model_types <- str_extract(model_filenames, "(decision_tree|logistic_regression|neural_network|xgboost)")
   in_bed_data_types <- str_extract(in_bed_data_filenames, "(decision_tree|logistic_regression|neural_network|xgboost)")
-  
-  # Pair filenames based on their types
   map(in_bed_data_types, ~ model_filenames[model_types == .x])
 }
 
-original_30_sec_data <- 
-  read_parquet("data/data_for_modelling/no_edge_sp_incl_features_30_sec_epochs.parquet")
-
-original_10_sec_data <- 
-  read_parquet("data/data_for_modelling/no_edge_sp_incl_features_10_sec_epochs.parquet")
-
-names_2nd_layer <- c("median_10", "median_5", "raw")
+get_preds <- function(in_bed_data_filenames, model_filenames) {
+  paired_filenames <- pair_filenames(model_filenames, in_bed_data_filenames)
+  map2(
+    in_bed_data_filenames, paired_filenames,
+    ~ map(.y, run_predictions, data_filename = .x)
+  ) %>%
+    set_names(c("in_bed_by_decision_tree", "in_bed_by_logistic_regression", "in_bed_by_neural_network", "in_bed_by_xgboost")) %>%
+    modify_depth(1, set_names, c("median_10", "median_5", "raw"))
+}
 
 # Set the paths for data and model files
 path_data <- "data/data_for_modelling/chained_classifiers/extracted_in_bed_data"
@@ -53,48 +46,78 @@ in_bed_data_10_sec_filenames <- get_filenames(path_data, "10_sec")
 model_30_sec_filenames <- get_filenames(path_models, "30_sec")
 model_10_sec_filenames <- get_filenames(path_models, "10_sec")
 
-# Pair 30 sec filenames
-paired_30_sec_filenames <- pair_filenames(model_30_sec_filenames, in_bed_data_30_sec_filenames)
+preds_30_sec <- get_preds(in_bed_data_30_sec_filenames, model_30_sec_filenames)
+preds_10_sec <- get_preds(in_bed_data_10_sec_filenames, model_10_sec_filenames)
 
-# Pair 10 sec filenames
-paired_10_sec_filenames <- pair_filenames(model_10_sec_filenames, in_bed_data_10_sec_filenames)
+# Combine the predictions into a tibble for each model
+extract_pred_class_by_model <- function(preds_list) {
+  map(preds_list, function(preds_method) {
+    # Combine all predictions into a tibble
+    preds_combined <- reduce(
+      map2(preds_method, names(preds_method), function(preds_type, preds_name) {
+        # The predictions are deeply nested, so you have to get to the actual tibble
+        preds_tibble <- preds_type[[1]]
 
-# For each pair of data filename and model filenames, apply `run_predictions`.
-preds_30_sec <- map2(
-  in_bed_data_30_sec_filenames,
-  paired_30_sec_filenames,
-  ~ map(.y, run_predictions, data_filename = .x)
-) %>%
-  # Set names for the second layer of the nested list
-  set_names(c("in_bed_by_decision_tree", "in_bed_by_logistic_regression", "in_bed_by_neural_network", "in_bed_by_xgboost")) %>%
-  modify_depth(1, set_names, names_2nd_layer)
+        # Select all columns from the predictions tibble
+        preds_column <- preds_tibble
 
-preds_10_sec <- map2(
-  in_bed_data_10_sec_filenames,
-  paired_10_sec_filenames,
-  ~ map(.y, run_predictions, data_filename = .x)
-) %>%
-  # Set names for the second layer of the nested list
-  set_names(c("in_bed_by_decision_tree", "in_bed_by_logistic_regression", "in_bed_by_neural_network", "in_bed_by_xgboost")) %>%
-  modify_depth(1, set_names, names_2nd_layer)
+        # Rename the column to match the name from the list it came from
+        preds_column <- rename(preds_column, !!str_c("sleep_", preds_name, "_pred_class") := .pred_class)
 
-join_predictions_to_original_data <- function(original_data, preds_list) {
-  # Iterate over each list of predictions
-  preds_joined <- map(preds_list, function(preds_method) {
-    map(preds_method, function(preds_type) {
-      # The predictions are deeply nested, so you have to get to the actual tibble
-      preds_tibble <- preds_type[[1]]
-      
-      # Join it to the original data
-      left_join(original_data, preds_tibble, by = c("id", "noon_day", "month", "datetime"))
-    })
+        preds_column
+      }),
+      inner_join,
+      by = c("id", "noon_day", "month", "datetime")
+    )
+
+    preds_combined
   })
-  
-  preds_joined
 }
 
-# Join the 30 sec predictions to the original 30 sec data
-joined_preds_30_sec <- join_predictions_to_original_data(original_30_sec_data, preds_30_sec)
+# Extract .pred_class columns from each tibble in preds_30_sec and combine them into tibbles by model
+pred_classes_30_sec_by_model <- extract_pred_class_by_model(preds_30_sec)
 
-# Join the 10 sec predictions to the original 10 sec data
-joined_preds_10_sec <- join_predictions_to_original_data(original_10_sec_data, preds_10_sec)
+complete_data_with_in_bed_preds_filenames <-
+  list.files("data/data_for_modelling/chained_classifiers", full.names = TRUE) %>%
+  str_subset("complete")
+
+walk2(
+  pred_classes_30_sec_by_model, complete_data_with_in_bed_preds_filenames,
+  ~ {
+    preds <- .x
+    data <- read_parquet(.y)
+    model_name <- str_extract(.y, "(?<=in_bed_median5_)(.*)(?=_30_sec_epoch_fit_predictions_complete)")
+
+    result <- right_join(preds, data,
+      by = c("id", "noon_day", "month", "datetime"),
+      relationship = "many-to-many"
+    ) %>%
+      mutate(
+        across(sleep_median_10_pred_class:sleep_raw_pred_class, ~ if_else(is.na(.x), as_factor(0), .x))
+      ) %>%
+      distinct()
+
+    output_file <- str_c("data/data_for_modelling/chained_classifiers/sleep_predictions/", model_name, "_sleep_prediction_complete.parquet")
+
+    write_parquet(result, output_file)
+
+    cat("Prediction data for model", model_name, "has been written to", output_file, "\n\n")
+  }
+)
+
+# test <- 
+#   read_parquet("data/data_for_modelling/chained_classifiers/sleep_predictions/xgboost_sleep_prediction_complete.parquet")
+# 
+# test %>% count(sleep_median_10_pred_class)
+
+# test %>%
+#   filter(id == 8505, noon_day == 9) %>%
+#   ggplot(aes(datetime, group = 1)) +
+#   geom_line(aes(y = as.numeric(sleep_median5))) +
+#   # geom_line(aes(y = as.numeric(sleep_median_10_pred_class) - .1), color = "darkred") +
+#   geom_line(aes(y = as.numeric(sleep_median_5_pred_class) - .2), color = "pink") +
+#   # geom_line(aes(y = as.numeric(sleep_raw_pred_class) - .3), color = "steelblue") +
+#   geom_line(aes(y = as.numeric(in_bed_class) - .5), color = "darkgreen") +
+#   geom_line(aes(y = as.numeric(in_bed) - .6), color = "darkblue") +
+#   facet_wrap(~ noon_day, scales = "free", ncol = 1) +
+#   theme_classic()
